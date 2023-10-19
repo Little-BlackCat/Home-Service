dc8c2b62b1e4cd4db4ef9e841d77ffb08d5d278c
BlackCat <neko.sword@gmail.com>
2023-10-19 13:02:41 +0700

    fix: strict mode

--- House-Service-Admin/src/main.tsx
+++ House-Service-Admin/src/main.tsx
@@ -1,19 +1,18 @@
-// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ProductProvider } from "./contexts/productsContext.tsx";
 import { Toaster } from './components/ui/toaster.tsx';
-
+import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  <Router>
-    <ProductProvider>
-      <App />
-      <Toaster />
-    </ProductProvider>
-  </Router>
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <Router>
+      <ProductProvider>
+        <App />
+        <Toaster />
+      </ProductProvider>
+    </Router>
+  </React.StrictMode>
 )
